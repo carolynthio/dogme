a5ea6c023c996e93214ae9d709511739ed9d79d7
Carolyn Thio <cthio@ucsd.edu>
2016-11-20 17:59:34 -0800

    Connected Django to Database on separate /list page

--- suggestedteams/models.py
+++ suggestedteams/models.py
@@ -5,13 +5,11 @@ from django.db import models
 # Create your models here.
 
 # This is a table
-class Student(models.Model):
+class Document(models.Model):
     # each of these are the columns in a table
     # name, datatype, constraint
-    name = models.CharField(max_length=140) # These can be found on the Djanga website
-    leadership = models.CharField(max_length=140)
-    schedule = models.TextField()
+    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
 
     # In order to list all the post titles making it a string
-    def __unicode__(self):
-        return self.name
+    # def __unicode__(self):
+    #     return self.name
