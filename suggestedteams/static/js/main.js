07fbb30a2d3f9052931194ac77bc3630f2a1c126
Carolyn Thio <cthio@ucsd.edu>
2016-11-27 09:37:40 -0800

    Added modal for constraints

--- suggestedteams/static/js/main.js
+++ suggestedteams/static/js/main.js
@@ -19,6 +19,30 @@ jQuery(document).ready(function($){
 	        $("#myOutput").html(data);
 	    }, "html");
 	});
+
+	/* Opening popup modal*/
+	var modal = document.getElementById('constraintModal');
+	var btn = document.getElementById("addConstraint");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+
+	btn.onclick = function() {
+  	modal.style.display = "block";
+	}
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+    modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+	}
+	/* End modal */
 });
 
 // Navigation Bar
